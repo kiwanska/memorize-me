7c8b8593de1059bd5dbcdf4e972c810de5443953
kiwanska <sobylmine@gmail.com>
2016-07-13 13:46:34 +0200

    rwd

--- js/app.js
+++ js/app.js
@@ -1,5 +1,58 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+	function orientation() {
+		var w = window,
+		    d = document,
+		    e = d.documentElement,
+		    g = d.getElementsByTagName('body')[0],
+		    width = w.innerWidth || e.clientWidth || g.clientWidth,
+		    height = w.innerHeight|| e.clientHeight|| g.clientHeight,
+		    board = document.querySelector('#board');
+		var ratio = width / height;
+		console.log(ratio);
+		if (ratio >= 1.5) {
+			board.style.height = '85vh';
+			board.style.width = '164vh';
+			var blocks = document.querySelectorAll('.block');
+			for (var i = 0; i < blocks.length; i++) {
+				blocks[i].style.height = '16vh';
+				blocks[i].style.width = '16vh';
+				blocks[i].style.padding = '1vh';
+    			blocks[i].style.margin = '1vh';
+			}
+		} 
+		else if (ratio >= 1 && ratio < 1.5) {
+			board.style.height = '85vh';
+			board.style.width = '164vh';
+			var blocks = document.querySelectorAll('.block');
+			for (var i = 0; i < blocks.length; i++) {
+				blocks[i].style.height = '16vh';
+				blocks[i].style.width = '16vh';
+				blocks[i].style.padding = '1vh';
+    			blocks[i].style.margin = '1vh';
+			}
+		}
+		else if (ratio >= 0.7 && ratio < 1) {
+
+		}
+		else if (ratio <= 0.7) {
+			var blocks = document.querySelectorAll('.block');
+			board.style.width = '85vw';
+			board.style.height = '166vw';
+			for (var i = 0; i < blocks.length; i++) {
+				blocks[i].style.height = '16vw';
+				blocks[i].style.width = '16vw';
+				blocks[i].style.padding = '1vw';
+    			blocks[i].style.margin = '1vw';
+			}
+		}
+	}
+
+	orientation();
+	window.onresize = orientation;
+	window.addEventListener('orientationchange', orientation());
+
+
 	var blocks = document.querySelectorAll('.block'),
 		again = document.querySelector('#again'),
 		myContent = [
@@ -22,6 +75,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			],
 		content = [],
 		activeBlocks = [],
+		timer,
 		active = 0,
 		points = 0,
 		moves = 0;
@@ -61,37 +115,46 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	for (var i = 0; i < blocks.length; i++) {
 		blocks[i].addEventListener('click', function(){
-			if (active < 2){
+			activeBlocks = document.querySelectorAll('.active');
+			active ++;
+			if (activeBlocks.length < 2){
 				this.classList.add('active');
 				this.innerHTML = this.dataset.inside;
-				active ++;
 			}
 			activeBlocks = document.querySelectorAll('.active');
-			if (active === 2) {
+			if (activeBlocks.length === 2 && active === 2) {
 				if (activeBlocks[0].dataset.id === activeBlocks[1].dataset.id) {
-					setTimeout(hit, 2000);
-					points ++;
+					timer = setTimeout(hit, 2000);
 				}
 				else {
-					setTimeout(missed, 2000);
+					timer = setTimeout(missed, 2000);
 				}
 				moves ++;
 
+			} else if (active > 2) {
+				if (activeBlocks[0].dataset.id === activeBlocks[1].dataset.id) {
+					window.clearTimeout(timer);
+					hit();	
+				}
+				else {
+					window.clearTimeout(timer);
+					missed();
+				}
+				moves ++;
+			} else if (activeBlocks.length === 1 && active === 2) {
+				active = 1;
 			}
-			if (points === 12) {
+			if (points === 16) {
 				console.log('PORTFOLIO');
 			}
 		});
 	}
 
-	// function score() {
-			
-	// }
-
 	function missed() {	
 		activeBlocks = document.querySelectorAll('.active');
 		for (var i = 0; i < activeBlocks.length; i++) {
-			activeBlocks[i].removeChild(document.querySelector('div.triangle'));
+			console.log(activeBlocks[i]);
+			activeBlocks[i].removeChild(activeBlocks[i].querySelector('.triangle'));
 			activeBlocks[i].classList.remove('active');
 			active = 0;
 		}
@@ -103,6 +166,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			activeBlocks[i].classList.remove('active');
 			activeBlocks[i].classList.add('gone');
 			active = 0;
+			points ++;
 		}
 	}
 
